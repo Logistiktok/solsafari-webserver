bcfde0b06f187e4da93707f4549b4373b0b9d6e4
Graunephar <daniel@welcomebob.com>
2021-04-04 01:04:53 +0200

    Added basic json functionality

--- index.py
+++ index.py
@@ -14,10 +14,12 @@ def hello_world():
 
 @app.route('/point/', methods=['GET', 'POST'])
 def point():
+    global hits
     if request.method == 'POST':
         req = request.get_json()
-        print(req["hits"])
+        hits = hits + req["hits"]
+        print(hits)
         return "Tak for point"
     else:
         point = 0
-        return render_template('point.html', point=point)
+        return render_template('point.html', point=hits)
